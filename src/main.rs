36a44f93f97baa37eee6251a6fddc3afaff2161a
freeelancer <lmengsplist96@gmail.com>
2024-06-09 19:11:58 +0800

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,65 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{env, path::PathBuf, process::Command};
+use std::{env, path::PathBuf, process};
+
+pub enum Command<'a> {
+    Exit(i32),
+    Echo(Vec<&'a str>),
+    Type(&'a str),
+    Cd(&'a str),
+    Pwd(),
+    NotBuiltin(&'a str, Vec<&'a str>),
+}
+
+pub fn into_command(raw_args: &str) -> Command {
+    let parsed_args: Vec<&str> = raw_args.split_whitespace().collect::<Vec<&str>>();
+    let cmd = parsed_args[0];
+    match cmd {
+        "exit" => Command::Exit(parsed_args[1].parse::<i32>().unwrap()),
+        "echo" => Command::Echo(parsed_args),
+        "type" => Command::Type(parsed_args[1]),
+        "pwd" => Command::Pwd(),
+        "cd" => Command::Cd(parsed_args.get(1).unwrap_or(&"")),
+        _ => Command::NotBuiltin(cmd, parsed_args[1..].to_vec()),
+    }
+}
+
+impl<'a> Command<'a> {
+    pub fn execute(self) {
+        match self {
+            Command::Exit(code) => std::process::exit(code),
+            Command::Echo(args) => {
+                let echo_string = args[1..].join(" ");
+                println!("{}", echo_string)
+            }
+            Command::Type(command) => match command {
+                "exit" | "echo" | "type" => println!("{} is a shell builtin", command),
+                _ => println!("{}", check_bin(command)),
+            },
+            Command::Cd(path) => {
+                if path == "~" {
+                    let home = env::var("HOME").unwrap();
+                    if let Err(err) = env::set_current_dir(home) {
+                        println!("{}", err.to_string());
+                    }
+                    return;
+                }
+                if let Err(_) = env::set_current_dir(path) {
+                    println!("cd: {}: No such file or directory", path);
+                }
+            }
+            Command::Pwd() => {
+                let current_dir = env::current_dir().unwrap();
+                println!("{}", current_dir.display());
+            }
+            Command::NotBuiltin(command, args) => {
+                if let Err(err) = run_bin(command, args) {
+                    println!("{}", err);
+                }
+            }
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,42 +74,8 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        let inputs: Vec<&str> = input.split_whitespace().collect();
-        match inputs[..] {
-            ["exit", "0"] => break,
-            ["echo", ..] => {
-                let echo_string = inputs[1..].join(" ");
-                println!("{}", echo_string);
-            }
-            ["type", arg] => match arg {
-                "exit" => println!("exit is a shell builtin"),
-                "echo" => println!("echo is a shell builtin"),
-                "type" => println!("type is a shell builtin"),
-                _ => println!("{}", check_bin(arg)),
-            },
-            ["pwd", ..] => {
-                let current_dir = env::current_dir().unwrap();
-                println!("{}", current_dir.display());
-            }
-            ["cd", path] => {
-                if path == "~" {
-                    let home = env::var("HOME").unwrap();
-                    if let Err(err) = env::set_current_dir(home) {
-                        println!("{}", err.to_string());
-                    }
-                    continue;
-                }
-                if let Err(_) = env::set_current_dir(path) {
-                    println!("cd: {}: No such file or directory", path);
-                }
-            }
-            [command, ..] => {
-                if let Err(err) = run_bin(command, inputs[1..].to_vec()) {
-                    println!("{}", err);
-                }
-            }
-            _ => println!("{}: command not found", inputs[0]),
-        }
+        let command = into_command(&input);
+        command.execute();
     }
 }
 
@@ -78,7 +103,7 @@ fn check_bin(file_name: &str) -> String {
 fn run_bin(file_name: &str, args: Vec<&str>) -> Result<(), String> {
     // get PATH environment variable
     if let Some(path) = find_exe(file_name) {
-        Command::new(path)
+        process::Command::new(path)
             .args(args)
             .status()
             .expect("failed to execute process");
