ea825d475a4b0da86732da180d7777a4ba618ab2
freeelancer <lmengsplist96@gmail.com>
2024-06-09 17:27:49 +0800

    added echo

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,24 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        // print command: command not found
-        let command = input.trim();
+        // Parse the input
+        let (command, arg) = parse_input(&input);
         if command == "exit 0" {
             break;
+        } else if command == "echo" {
+            println!("{}", arg);
+        } else {
+            println!("{}: command not found", command);
         }
-        println!("{}: command not found", command);
     }
+}
+
+fn parse_input(input: &str) -> (&str, &str) {
+    let iter: Vec<&str> = input.split_whitespace().collect();
+    if iter.len() == 0 {
+        return ("", "");
+    } else if iter.len() == 1 {
+        return (iter[0].trim(), "");
+    }
+    return (iter[0].trim(), iter[1].trim());
 }
