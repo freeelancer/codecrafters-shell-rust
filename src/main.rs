d97f5f8705863646e4436f1e2d5e3afc2be9d329
freeelancer <lmengsplist96@gmail.com>
2024-06-09 17:19:10 +0800

    add loop

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,17 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    // print command: command not found
-    let command = input.trim();
-    println!("{}: command not found", command);
+        // print command: command not found
+        let command = input.trim();
+        println!("{}: command not found", command);
+    }
 }
