d2c97c51da262b9e1f37bb7fe5a3fb8787ed81b2
freeelancer <lmengsplist96@gmail.com>
2024-06-09 17:46:00 +0800

    added ls

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,33 @@ fn main() {
             ["echo", ..] => {
                 let echo_string = inputs[1..].join(" ");
                 println!("{}", echo_string);
-            },
-            ["type", command] => {
-                match command {
-                    "exit" => println!("exit is a shell builtin"),
-                    "echo" => println!("echo is a shell builtin"),
-                    "type" => println!("type is a shell builtin"),
-                    _ => println!("{} not found", command),
-                }
             }
+            ["type", arg] => match arg {
+                "exit" => println!("exit is a shell builtin"),
+                "echo" => println!("echo is a shell builtin"),
+                "type" => println!("type is a shell builtin"),
+                "ls" => ls(arg),
+                _ => println!("{} not found", arg),
+            },
             _ => println!("{}: command not found", inputs[0]),
         }
     }
+}
+
+fn ls(file_name: &str) {
+    // Implement the ls command here
+    // get PATH environment variable
+    let paths_env: String = std::env::var("PATH").unwrap();
+    // split the path into a vector of paths
+    let paths: Vec<&str> = paths_env.split(":").collect();
+    // iterate over the paths
+    for path in paths {
+        // check if the file exists in the path
+        let file_path = format!("{}/{}", path, file_name);
+        if std::fs::metadata(file_path).is_ok() {
+            println!("{} is {}", file_name, file_path);
+            return;
+        }
+    }
+    println!("{}: command not found", file_name);
 }
